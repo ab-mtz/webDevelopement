c54ce81ab267f6f731cae4c808b80ff2b4790a7a
abrahamMartinez <bmtzs@me.com>
2023-08-11 15:21:45 +0200

    Syntax

--- Express/EJS_test/index.js
+++ Express/EJS_test/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')) //Instead the current directory, go to the specified directory
 
 app.get('/', (req, res) => { 
     res.render('home.ejs')
