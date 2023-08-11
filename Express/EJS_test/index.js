2dd0910034cfcf52e9b8dbfb0ac97cd86eccae18
abrahamMartinez <bmtzs@me.com>
2023-08-11 15:28:45 +0200

    Passing data to Template

--- Express/EJS_test/index.js
+++ Express/EJS_test/index.js
@@ -6,7 +6,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views')) //Instead the current directory, go to the specified directory
 
 app.get('/', (req, res) => { 
-    res.render('home.ejs')
+    res.render('home')
+})
+
+app.get('/rand', (req, res) => { 
+    const num = Math.floor(Math.random() *10) + 1;
+    res.render('random', { rand: num })  //We could just write num an that would tell to use num as num
 })
 
 app.listen(3000, () => {
