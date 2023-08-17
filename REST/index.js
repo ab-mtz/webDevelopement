8c671dcbbac56901d5b3584ea068e13075b70013
abrahamMartinez <bmtzs@me.com>
2023-08-17 15:09:09 +0200

    Rendering existing comments

--- REST/index.js
+++ REST/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const path = require('path');
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -31,13 +31,15 @@ const comments = [
         },
 ];
 
-app.get('/comments', (req, res) => {
-    res.render('/comments/index')
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
 })
 
-app.get('/getpost', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
