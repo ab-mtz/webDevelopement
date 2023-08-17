240559819eee3876ae3d5c91d2545b4aab38814c
abrahamMartinez <bmtzs@me.com>
2023-08-17 14:37:28 +0200

    ejs

--- REST/index.js
+++ REST/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/getpost', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// GET /comments - list all comments
+// POST /comments - create a new comments
+// GET  /comments/:id - get one comment (using ID)
+// PATCH /comments/:id - Delete comment
