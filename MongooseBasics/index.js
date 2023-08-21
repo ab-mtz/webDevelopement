8e5bb3125c4ae669161c6974854d1c6079695208
abrahamMartinez <bmtzs@me.com>
2023-08-21 14:53:58 +0200

    new

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -7,3 +7,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/test')
         console.log("error")
         console.log(err)
     })
+
+    //We have to create a schema that will be use in the DB
+
+    const movieSchema = new mongoose.Schema({
+        title: String,
+        year: Number,
+        score: Number,
+        rating: String,
+    })
+
+    //Creating a Movie class
+    const Movie = mongoose.model('Movie', movieSchema);
+    //Making a new instance of the Movie class
+    const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'});
