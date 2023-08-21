16067cad16a461a23b20249bd2431fcbc2d4468c
abrahamMartinez <bmtzs@me.com>
2023-08-21 20:49:24 +0200

    updating

--- MongooseBasics/products.js
+++ MongooseBasics/products.js
@@ -13,19 +13,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
     const productSchema = new mongoose.Schema({
         name: { 
             type: String,
-            required: true
+            required: true,
+            maxlenght: 20
         },
         price: {
             type: Number,
-            // require: true
-        }
+            require: true,
+            min: 0
 
+        },
+        onSale: { 
+            type: Boolean,
+            default: false
+        },
+        categories: [String],
+        qty: {
+            online: {
+                type: Number,
+                default: 0
+            },
+            inStore: {
+                type: Number,
+                default: 0
+            }
+        }
     });
 
     const Product = mongoose.model('Product', productSchema);
 
-    const bike = new Product({ price: 599})    //name: 'Mountain Bike',
-    bike.save()
+    // const bike = new Product({ name: 'Tire Pump', price: 19.90})    
+    // bike.save()
+    //     .then(data => {
+    //         console.log("Worked");
+    //         console.log(data);
+    //     })
+    //     .catch(err => {
+    //         console.log("Error");
+    //         console.log(err.errors);
+    //     })
+
+    // Update
+    Product.findOneAndUpdate({ name: 'Tire Pump'}, {onSale: true, qty{online: 3}}, {new: true, runValidators: true})    
+    // bike.save()
         .then(data => {
             console.log("Worked");
             console.log(data);
