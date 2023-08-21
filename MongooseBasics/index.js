a4c0da7b182954921ff9210680031af0e94d552f
abrahamMartinez <bmtzs@me.com>
2023-08-21 15:12:28 +0200

    insert many

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -20,4 +20,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/test')
     //Creating a Movie class
     const Movie = mongoose.model('Movie', movieSchema);
     //Making a new instance of the Movie class
-    const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'});
+    // const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+    Movie.insertMany([
+        { title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' },   
+        { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },   
+        { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },   
+        { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },   
+        { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }   
+    ])
+        .then(data => {
+            console.log("It worked");
+            console.log(data);
+        })
