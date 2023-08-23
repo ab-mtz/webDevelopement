bcaedf9131f7f850e2abfc033c63572a4e0037cb
abrahamMartinez <bmtzs@me.com>
2023-08-23 19:19:30 +0200

    delete

--- mongooseWithExpress/index.js
+++ mongooseWithExpress/index.js
@@ -62,6 +62,13 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`);
 })
 
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products');
+})
+
+
 app.listen(3000, () => {
     console.log("App listening on port 3000")
 })
