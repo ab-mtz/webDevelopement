cddad1171623da2df879a3de1c0b839425640e4d
abrahamMartinez <bmtzs@me.com>
2023-08-23 16:17:45 +0200

    products lodaded

--- mongooseWithExpress/index.js
+++ mongooseWithExpress/index.js
@@ -17,6 +17,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+//An Async callback for a route where we await some moongose operation 
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    console.log(products)
+    res.render('products/index')
+})
+
 app.listen(3000, () => {
     console.log("App listening on port 3000")
 })
