1d17bbb98fccc52e33ed5d6dbdb28c3572633b85
abrahamMartinez <bmtzs@me.com>
2023-08-26 14:50:41 +0200

    morgan

--- morganMiddleware/index.js
+++ morganMiddleware/index.js
@@ -1,5 +1,10 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import morgan from 'morgan';
+
+// app.use allow us to run code in everey single request
+app.use(morgan('tiny')
+)
 
 app.get('/', (req, res)  => {
     res.send('Homepage')
